470a9287a5c16f24d076212e9ddf40b2e57763d9
Roman Zinatulin <zrf_123@mail.ru>
2024-10-07 17:50:37 +0300

    task 2 done

--- db/db.go
+++ db/db.go
@@ -2,16 +2,21 @@ package db
 
 import (
 	"database/sql"
-	"errors"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
-	_ "modernc.org/sqlite"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
+
+type Task struct {
+	ID      string `json:"id"`
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Repeat  string `json:"repeat"`
+}
 
 func Init() {
 	dbFile := os.Getenv("DB_FILE")
@@ -22,4 +27,45 @@ func Init() {
 	log.Printf("Используемый файл БД: %s", dbFile)
 
 	var install bool
+
+	_, err := os.Stat(dbFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			install = true
+		} else {
+			log.Fatalf("Не удалось проверить существует ли файл БД: %v", err)
+		}
+	}
+
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = db
+
+	if install {
+		log.Println("Создаем БД")
+
+
+		_, err := DB.Exec(`CREATE TABLE IF NOT EXISTS scheduler (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL CHECK(length(date) = 8),
+            title TEXT NOT NULL,
+            comment TEXT,
+            repeat TEXT CHECK(length(repeat) <= 128)
+        );`)
+		if err != nil {
+			log.Fatalf("Ошибка при создании таблицы: %v", err)
+		}
+
+		_, err = DB.Exec(`CREATE INDEX IF NOT EXISTS idx_date ON scheduler(date);`)
+		if err != nil {
+			log.Fatalf("Ошибка при создании индекса: %v", err)
+		}
+
+		log.Println("БД создана")
+	} else {
+		log.Println("БД уже существует")
+	}
 }
--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"go_final_project/db"
 )
 
 func main() {
+
+	db.Init()
+	defer db.DB.Close()
+
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 	err := http.ListenAndServe(":7540", nil)
 	if err != nil {
